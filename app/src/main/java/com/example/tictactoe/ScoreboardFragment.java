5bb30801fd34370c31ff1c8ffe640367b078f9e2
lilOAT <99389031+lilOAT@users.noreply.github.com>
2023-08-30 19:18:32 +0800

    Scoreboard Fragment file rename
    Renamed Scoreboard.java -> ScoreboardFragment.java

--- app/src/main/java/com/example/tictactoe/ScoreboardFragment.java
+++ app/src/main/java/com/example/tictactoe/ScoreboardFragment.java
@@ -15,10 +15,10 @@ import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link Scoreboard#newInstance} factory method to
+ * Use the {@link ScoreboardFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class Scoreboard extends Fragment {
+public class ScoreboardFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -29,7 +29,7 @@ public class Scoreboard extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public Scoreboard() {
+    public ScoreboardFragment() {
         // Required empty public constructor
     }
 
@@ -39,11 +39,11 @@ public class Scoreboard extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment Scoreboard.
+     * @return A new instance of fragment ScoreboardFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static Scoreboard newInstance(String param1, String param2) {
-        Scoreboard fragment = new Scoreboard();
+    public static ScoreboardFragment newInstance(String param1, String param2) {
+        ScoreboardFragment fragment = new ScoreboardFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
